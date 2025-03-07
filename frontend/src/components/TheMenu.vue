38771575ace9a92e64b1701ed73f71cafd7f541e
Oliver Larsson <larsson.e.oliver@gmail.com>
2025-03-07 16:07:21 +0100

    Remove Prime, going native instead

--- frontend/src/components/TheMenu.vue
+++ frontend/src/components/TheMenu.vue
@@ -1,29 +1,18 @@
 <template>
   <div>
-    <Menubar :model="items">
-      <template #item="{ item, props }">
-        <a v-ripple class="flex items-center" v-bind="props.action">
-          <span v-if="item.icon" :class="item.icon"></span>
-          <span>{{ item.label }}</span>
-        </a>
-      </template>
-      <template #end>
-        <Avatar icon="pi pi-user" shape="circle" size="normal" />
-      </template>
-    </Menubar>
+    Test
+    <!-- <Menubar :model="items"> -->
+    <!--   <template #item="{ item, props }"> -->
+    <!--     <a v-ripple class="flex items-center" v-bind="props.action"> -->
+    <!--       <span v-if="item.icon" :class="item.icon"></span> -->
+    <!--       <span>{{ item.label }}</span> -->
+    <!--     </a> -->
+    <!--   </template> -->
+    <!--   <template #end> -->
+    <!--     <Avatar icon="pi pi-user" shape="circle" size="normal" /> -->
+    <!--   </template> -->
+    <!-- </Menubar> -->
   </div>
 </template>
 
-
-<script setup lang="ts">
-import type { MenuItem } from "primevue/menuitem";
-import { ref } from "vue";
-
-const items = ref<MenuItem[]>([
-  {
-    label: 'Home',
-    icon: 'pi pi-home'
-  },
-]);
-
-</script>
+<script setup lang="ts"></script>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,17 +4,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import Components from "unplugin-vue-components/vite";
-import { PrimeVueResolver } from "@primevue/auto-import-resolver";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    Components({
-      resolvers: [PrimeVueResolver()],
-    }),
-  ],
+  plugins: [vue(), vueDevTools(), Components({})],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
