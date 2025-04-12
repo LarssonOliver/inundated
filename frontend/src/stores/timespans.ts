4c3b019895ed34036a85e8476277233a4cf0b910
Oliver Larsson <larsson.e.oliver@gmail.com>
2025-04-12 11:26:31 +0200

    Store work

--- frontend/src/stores/timespans.ts
+++ frontend/src/stores/timespans.ts
@@ -5,36 +5,108 @@ import { ref } from "vue";
 
 export const useTimeSpansStore = defineStore("timeSpans", () => {
   const timeSpans = ref<TimeSpan[]>([
-    {
-      id: 1,
-      name: "Test 1",
-      startTime: new Date("2019-01-02T12:40:00.000Z"),
-      endTime: new Date("2019-01-02T13:30:00.000Z"),
-      timeZone: "Europe/Stockholm",
-      userId: 1,
-      tagIds: [],
-    },
-    {
-      id: 2,
-      name: "Test 2",
-      startTime: new Date("2019-01-02T14:40:00.000Z"),
-      endTime: new Date("2019-01-02T18:30:00.000Z"),
-      timeZone: "Europe/Stockholm",
-      userId: 1,
-      tagIds: [],
-    },
-    {
-      id: 3,
-      name: "Test 3",
-      startTime: new Date("2019-01-02T11:40:00.000Z"),
-      endTime: new Date("2019-01-02T12:30:00.000Z"),
-      timeZone: "Europe/Stockholm",
-      userId: 1,
-      tagIds: [],
-    },
+    // {
+    //   id: 1,
+    //   name: "Test 1",
+    //   startTime: new Date("2019-01-02T12:40:00.000Z"),
+    //   endTime: new Date("2019-01-02T13:30:00.000Z"),
+    //   timeZone: "Europe/Stockholm",
+    //   userId: 1,
+    //   tagIds: [],
+    // },
+    // {
+    //   id: 2,
+    //   name: "Test 2",
+    //   startTime: new Date("2019-01-02T14:40:00.000Z"),
+    //   endTime: new Date("2019-01-02T18:30:00.000Z"),
+    //   timeZone: "Europe/Stockholm",
+    //   userId: 1,
+    //   tagIds: [],
+    // },
+    // {
+    //   id: 3,
+    //   name: "Test 3",
+    //   startTime: new Date("2019-01-02T11:40:00.000Z"),
+    //   endTime: new Date("2019-01-02T12:30:00.000Z"),
+    //   timeZone: "Europe/Stockholm",
+    //   userId: 1,
+    //   tagIds: [],
+    // },
   ]);
 
-  return { timeSpans };
+  /**
+  * Creates a new time span.
+  *
+  * @param timeSpan - The time span to create.
+  *
+  * @returns A promise that resolves to the newly created time span with
+  *   correctly assigned ID.
+  */
+  async function createTimeSpan(timeSpan: TimeSpan): Promise<TimeSpan> {
+    const newTimeSpan: TimeSpan = {
+      id: timeSpans.value.length + 1, // TODO: Use ID from server
+      name: timeSpan.name,
+      startTime: new Date(timeSpan.startTime),
+      endTime: new Date(timeSpan.endTime),
+      timeZone: timeSpan.timeZone,
+      userId: timeSpan.userId, // TODO: Use the current user ID
+      tagIds: [...timeSpan.tagIds],
+    };
+
+    // TODO: Implement API call to create the time span on the server
+
+    timeSpans.value.push(newTimeSpan);
+    return newTimeSpan;
+  }
+
+  /**
+  * Fetches a timespan by its ID.
+  *
+  * @param id - The ID of the time span to fetch.
+  *
+  * @returns A promise that resolves to the time span with the given ID,
+  *   or undefined if not found.
+  */
+  async function getTimeSpanById(id: number): Promise<TimeSpan | undefined> {
+    const timeSpan = timeSpans.value.find((ts) => ts.id === id);
+
+    if (!timeSpan)
+      return undefined;
+
+    const copy: TimeSpan = {
+      ...timeSpan,
+      startTime: new Date(timeSpan.startTime),
+      endTime: new Date(timeSpan.endTime),
+      tagIds: [...timeSpan.tagIds],
+    };
+
+    return copy;
+  }
+
+  /**
+  * Updates an existing time span.
+  *
+  * @param timeSpan - The time span to update.
+  *
+  * @returns A promise that resolves to the updated time span.
+  */
+  async function updateTimeSpan(timeSpan: TimeSpan): Promise<TimeSpan> {
+    throw new Error("Not implemented");
+  }
+
+  /**
+  * Deletes a time span.
+  *
+  * @param id - The ID of the time span to delete.
+  *
+  * @returns A promise that resolves when the time span is deleted.
+  */
+  async function deleteTimeSpan(id: number): Promise<void> {
+    const index = timeSpans.value.findIndex((ts) => ts.id === id);
+    timeSpans.value.splice(index, 1);
+  }
+
+  return { timeSpans, getTimeSpanById, createTimeSpan, updateTimeSpan, deleteTimeSpan };
 });
 
 if (import.meta.hot) {
---
+++ frontend/src/stores/timesspans.test.ts
@@ -0,0 +1,85 @@
+import { useTimeSpansStore } from "@/stores/timespans";
+import { setActivePinia, createPinia } from "pinia";
+import { test, expect, beforeEach } from "vitest";
+
+const timeSpan = {
+  id: 0,
+  name: "Test",
+  startTime: new Date("2019-01-02T12:40:00.000Z"),
+  endTime: new Date("2019-01-02T13:30:00.000Z"),
+  timeZone: "Europe/Stockholm",
+  userId: 1,
+  tagIds: [1, 2, 5],
+};
+
+beforeEach(() => {
+  setActivePinia(createPinia());
+});
+
+test("Store empty on init", () => {
+  const store = useTimeSpansStore();
+  expect(store.timeSpans.length).toBe(0);
+});
+
+test("Create time span", async () => {
+  const store = useTimeSpansStore();
+  const ts = await store.createTimeSpan(timeSpan);
+  expect(ts).not.toEqual(timeSpan);
+  expect(ts.id).toBeGreaterThan(0);
+  expect(ts.startTime).toEqual(timeSpan.startTime);
+  expect(ts.endTime).toEqual(timeSpan.endTime);
+  expect(ts.timeZone).toEqual(timeSpan.timeZone);
+  expect(ts.tagIds).toEqual(timeSpan.tagIds);
+});
+
+test("Create timespan unlinks arrays", async () => {
+  const store = useTimeSpansStore();
+  const ts = await store.createTimeSpan(timeSpan);
+  ts.tagIds.push(3);
+  expect(ts.tagIds).not.toEqual(timeSpan.tagIds);
+});
+
+test("Create timespan unlinks date objects", async () => {
+  const store = useTimeSpansStore();
+  const ts = await store.createTimeSpan(timeSpan);
+  ts.startTime.setSeconds(timeSpan.startTime.getSeconds() + 1);
+  ts.endTime.setSeconds(timeSpan.endTime.getSeconds() + 1);
+  expect(ts.startTime).not.toEqual(timeSpan.startTime);
+  expect(ts.endTime).not.toEqual(timeSpan.endTime);
+});
+
+test("Get timespan by id", async () => {
+  const store = useTimeSpansStore();
+  const ts = await store.createTimeSpan(timeSpan);
+  const ts2 = await store.getTimeSpanById(ts.id);
+  expect(ts2).not.toBeUndefined();
+});
+
+test("Get timespan by id not found", async () => {
+  const store = useTimeSpansStore();
+  expect(await store.getTimeSpanById(0)).toBeUndefined();
+  expect(await store.getTimeSpanById(-1)).toBeUndefined();
+  expect(await store.getTimeSpanById(11)).toBeUndefined();
+});
+
+test("Get timespan is decoupled from store", async () => {
+  const store = useTimeSpansStore();
+  const ts = await store.createTimeSpan(timeSpan);
+  const ts2 = await store.getTimeSpanById(ts.id);
+  expect(ts2).not.toBeUndefined();
+  ts2?.tagIds.push(3);
+  ts2?.startTime.setSeconds(ts2.startTime.getSeconds() + 1);
+  ts2?.endTime.setSeconds(ts2.endTime.getSeconds() + 1);
+  expect(ts2?.tagIds).not.toEqual(ts.tagIds);
+  expect(ts2?.startTime).not.toEqual(ts.startTime);
+  expect(ts2?.endTime).not.toEqual(ts.endTime);
+});
+
+test("Get timespan by id not found after delete", async () => {
+  const store = useTimeSpansStore();
+  const ts = await store.createTimeSpan(timeSpan);
+  await store.deleteTimeSpan(ts.id);
+  expect(await store.getTimeSpanById(ts.id)).toBeUndefined();
+});
+
+
