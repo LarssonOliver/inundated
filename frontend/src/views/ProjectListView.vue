388b6e9f97bb7bedcba6a73f94d8a727a6ab07fb
Oliver Larsson <larsson.e.oliver@gmail.com>
2026-01-14 20:39:22 +0100

    Start project list

--- frontend/src/views/ProjectListView.vue
+++ frontend/src/views/ProjectListView.vue
@@ -1,6 +1,9 @@
 <template>
-  <div v-for="project in projectsStore.projects" :key="project.id">
-    {{ project }}
+  <div class="project-list">
+    <h2>Projects</h2>
+    <div v-for="project in projectsStore.projects" :key="project.id" class="project-item">
+      {{ project }}
+    </div>
   </div>
 </template>
 
@@ -8,6 +11,26 @@
 import { useProjectsStore } from "@/stores/projects";
 
 const projectsStore = useProjectsStore();
+
+projectsStore.createProject({
+  id: 0,
+  name: "Test",
+  color: "#FF0000",
+  timeBudget: 60,
+  userId: 1,
+  tagIds: [1, 2, 5],
+});
 </script>
 
-<style scoped></style>
+<style scoped>
+.project-list {
+  margin: 0 1em;
+}
+
+.project-item {
+  background-color: var(--nord0);
+  padding: 0.5em;
+  border-radius: 4px;
+  border: 1px solid var(--nord1);
+}
+</style>
