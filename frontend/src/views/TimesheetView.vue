60508b73f65d7bb997de8a6c6a5d550d699db696
Oliver Larsson <larsson.e.oliver@gmail.com>
2026-01-16 08:21:10 +0100

    Small timesheet edit

--- frontend/src/views/TimesheetView.vue
+++ frontend/src/views/TimesheetView.vue
@@ -1,23 +1,8 @@
 <template>
-  <div style="display: flex">
-    <TagItem :tag="items[0]" />
-    <TagItem :tag="items[1]" />
-    <TagItem :tag="items[2]" />
-  </div>
-
-  <hr />
-
+  <h2>Timesheet</h2>
   <TheTimesheet />
 </template>
 
 <script setup lang="ts">
-import TagItem from "@/components/tags/TagItem.vue";
 import TheTimesheet from "@/components/timesheet/TheTimesheet.vue";
-import type { Tag } from "@/model/tag";
-
-const items: Tag[] = [
-  { id: 1, name: "tag:test", color: "#44b", userId: 1 },
-  { id: 2, name: "tag:hello", color: "#bbb", userId: 1 },
-  { id: 3, name: "tag:world", color: "#acd", userId: 1 },
-];
 </script>
