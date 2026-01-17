6e92a64e75ec929f41a8afcdf1d5c8498c2d9ba2
Oliver Larsson <larsson.e.oliver@gmail.com>
2026-01-17 16:48:25 +0100

    CSS

--- frontend/src/views/TimesheetView.vue
+++ frontend/src/views/TimesheetView.vue
@@ -1,8 +1,16 @@
 <template>
-  <h2>Timesheet</h2>
-  <TheTimesheet />
+  <div class="timesheet-view">
+    <h2>Timesheet</h2>
+    <TheTimesheet />
+  </div>
 </template>
 
 <script setup lang="ts">
 import TheTimesheet from "@/components/timesheet/TheTimesheet.vue";
 </script>
+
+<style scoped>
+.timesheet-view {
+  margin: 0 1em;
+}
+</style>
