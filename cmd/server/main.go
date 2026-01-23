cffd327a797223aee761e4f2bbd7afddfe879c18
Oliver Larsson <larsson.e.oliver@gmail.com>
2026-01-23 10:52:22 +0100

    Simple http middleware logging

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/larssonoliver/inundated/internal/api"
 	"github.com/larssonoliver/inundated/internal/api/handlers"
+	"github.com/larssonoliver/inundated/internal/middleware"
 	"github.com/larssonoliver/inundated/internal/repository"
 	"github.com/larssonoliver/inundated/internal/repository/memory"
 	"github.com/larssonoliver/inundated/internal/service"
@@ -24,6 +26,9 @@ func main() {
 
 	r := chi.NewMux()
 
+	logger := log.New(os.Stdout, "[http] ", log.LstdFlags)
+	r.Use(middleware.RequestLogger(logger))
+
 	h := api.HandlerFromMux(api.NewStrictHandler(server, nil), r)
 
 	s := &http.Server{
@@ -31,5 +36,6 @@ func main() {
 		Addr:    "0.0.0.0:8080",
 	}
 
+	log.Println("Starting server on :8080")
 	log.Fatal(s.ListenAndServe())
 }
---
+++ internal/middleware/logging.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func RequestLogger(logger *log.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+
+			next.ServeHTTP(ww, r)
+
+			logger.Printf(
+				"%s %s %d %dB %s",
+				r.Method,
+				r.URL.Path,
+				ww.Status(),
+				ww.BytesWritten(),
+				time.Since(start),
+			)
+		})
+	}
+}
