e8909dc68d9f324e0884525edee116a4ffc6f1d6
Oliver Larsson <larsson.e.oliver@gmail.com>
2026-05-23 09:08:29 +0200

    Add default pagination helper

--- internal/model/pagination.go
+++ internal/model/pagination.go
@@ -1,5 +1,12 @@
 package model
 
+func DefaultPaginationParams() PaginationParams {
+	return PaginationParams{
+		Limit:  25, // From OpenAPI spec
+		Offset: 0,
+	}
+}
+
 type PaginationParams struct {
 	Limit  int
 	Offset int
