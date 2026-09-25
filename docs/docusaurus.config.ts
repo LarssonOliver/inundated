f2acf939859dcf819ac4b3a48325beef66308812
Oliver Larsson <larsson.e.oliver@gmail.com>
2026-09-25 10:24:25 +0200

    fix(docs): update root site address

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -49,7 +49,7 @@ const config: Config = {
     v4: true,
   },
 
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://inundated.app',
   baseUrl: '/',
 
   organizationName: 'LarssonOliver',
